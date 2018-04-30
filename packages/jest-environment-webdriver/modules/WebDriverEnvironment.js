aac543c583674f201867124d7381ee0874b8c439
John Maschmeyer <jrm@conduce.com>
2018-04-30 10:27:13 -0500

    Add option for headless browsers

--- packages/jest-environment-webdriver/modules/WebDriverEnvironment.js
+++ packages/jest-environment-webdriver/modules/WebDriverEnvironment.js
@@ -1,4 +1,6 @@
 const NodeEnvironment = require('jest-environment-node');
+const chrome = require('selenium-webdriver/chrome');
+const firefox = require('selenium-webdriver/firefox');
 const { Builder, By, until } = require('selenium-webdriver');
 
 class WebDriverEnvironment extends NodeEnvironment {
@@ -6,17 +8,26 @@ class WebDriverEnvironment extends NodeEnvironment {
     super(config);
     const options = config.testEnvironmentOptions || {};
     this.browserName = options.browser || 'chrome';
+    this.headless = options.headless;
     this.seleniumAddress = options.seleniumAddress || null;
   }
 
   async setup() {
     await super.setup();
-    
+
     let driver = new Builder();
     if (this.seleniumAddress) {
       driver = driver.usingServer(this.seleniumAddress);
     }
-    driver = await driver.forBrowser(this.browserName).build();
+    if (this.headless) {
+      driver = await driver
+        .forBrowser(this.browserName)
+        .setChromeOptions(new chrome.Options().headless())
+        .setFirefoxOptions(new firefox.Options().headless())
+        .build();
+    } else {
+      driver = await driver.forBrowser(this.browserName).build();
+    }
 
     this.driver = driver;
 
